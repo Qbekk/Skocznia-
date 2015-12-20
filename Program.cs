a7d58c7d2dd1bd8f715d5a36023350386d696880
Qbekk <Qbek@gd.pl>
2015-12-20 16:11:07 +0100

    Dodawanie skoczków
    Można wprowadzać skoczków , wyświetlają się poprawnie

--- Program.cs
+++ Program.cs
@@ -10,10 +10,30 @@ namespace wat
     {
         static void Main(string[] args)
         {
-        	Console.SetWindowSize(120,55);
+            int rozmiar, gracze, skill;
+            string imie;
+            Console.WriteLine("Witaj! Podaj rozmiar skoczni!");
+            rozmiar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj ilość skoczków!");
+            gracze = int.Parse(Console.ReadLine());
+            Skoczek[] tabela = new Skoczek[gracze];
+            for (int i = 0; i< gracze; i ++)
+                {
+                    Console.WriteLine("Podaj imię skoczka nr {0}",i+1);
+                    imie = Console.ReadLine();
+                    Console.WriteLine("Podaj poziom umiejętności (między 1, a 10) skoczka nr {0}", i+1);
+                    skill = int.Parse(Console.ReadLine());
+                    tabela[i] = new Skoczek(imie,skill);
+                }
+            for (int i = 0; i < gracze; i++)
+            {
+                Console.WriteLine("Skoczek {0} nazywa się {1} i ma poziom umiejętności {2}",i+1,tabela[i].name, tabela[i].skill);
+            }
+            /*
+            Console.SetWindowSize(120,55);
             var wut = new Skocznia(12);
             wut.buduj();
-            wut.zjazd(4);
+            wut.zjazd(4);*/
             Console.ReadKey();
             
         }
---
+++ skoczek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wat
+{
+    class Skoczek
+    {
+        public int skill;
+        public string name;
+        public Skoczek(string name, int skill)
+        {
+            this.skill = skill;
+            this.name = name;
+        }
+
+
+
+
+    }
+}
