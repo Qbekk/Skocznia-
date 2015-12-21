68455a291ff66138eec49596877bb697a68b1ffd
Qbekk <qbek@gd.pl>
2015-12-21 09:52:21 +0100

    co znacza te czerwone rzeczy?

---
+++ skok.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace wat
+{
+	public class Skok
+	{
+		public int numer,odleglosc,punkty;
+		
+		
+		public Skok(int numer, int odleglosc, int punkty)
+		{
+			this.numer=numer;
+			this.odleglosc=odleglosc;			
+			this.punkty=punkty;
+		}
+	}
+}
